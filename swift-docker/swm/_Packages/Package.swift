d515146a9cc4b9136418c4792c338fbeb696c322
Taketo Sano <taketo1024@gmail.com>
2021-05-24 16:43:23 +0900

    add swm workflows etc.

---
+++ swift-docker/swm/_Packages/Package.swift
@@ -0,0 +1,57 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "_Packages",
+    products: [
+        .library(name: "_Packages", type: .dynamic, targets: ["_Packages"]),
+    ],
+    dependencies: [
+        .package(
+			url: "https://github.com/taketo1024/swm-core.git",
+			from:"1.0.1"
+		),
+        .package(
+			url: "https://github.com/taketo1024/swm-knots.git",
+			from: "1.0.0"
+		),
+        .package(
+			url: "https://github.com/taketo1024/swm-topology.git",
+			from: "1.0.0"
+		),
+        .package(
+			url: "https://github.com/taketo1024/swm-homology.git",
+			from: "1.0.0"
+		),
+        .package(
+			url: "https://github.com/taketo1024/swm-khovanov.git",
+			from: "1.0.0"
+		),
+        .package(
+			url: "https://github.com/taketo1024/swm-hfk.git",
+			from: "1.0.0"
+		),
+        .package(
+			url: "https://github.com/taketo1024/swm-kr.git",
+			from: "1.0.0"
+		),
+    ],
+    targets: [
+        .target(
+            name: "_Packages",
+            dependencies: [
+                .product(name: "SwmCore", package: "swm-core"),
+                .product(name: "SwmKnots", package: "swm-knots"),
+                .product(name: "SwmHomology", package: "swm-homology"),
+                .product(name: "SwmKhovanov", package: "swm-khovanov"),
+                .product(name: "SwmHFK", package: "swm-hfk"),
+                .product(name: "SwmKR", package: "swm-kr"),
+			]
+            swiftSettings: [
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+            ]
+		),
+    ]
+)
