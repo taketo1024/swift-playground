34c6049ba534aa578baf1d11320da29fda276219
Taketo Sano <taketo1024@gmail.com>
2021-06-26 17:14:03 +0900

    update versions.

--- swift-docker/swm/_Packages/Package.swift
+++ swift-docker/swm/_Packages/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(
 			url: "https://github.com/taketo1024/swm-core.git",
-			from:"1.2.6"
+			from:"1.2.7"
 		),
         .package(
 			url: "https://github.com/taketo1024/swm-knots.git",
@@ -19,15 +19,15 @@ let package = Package(
 		),
         .package(
 			url: "https://github.com/taketo1024/swm-homology.git",
-			from: "1.3.0"
+			from: "1.3.1"
 		),
         .package(
 			url: "https://github.com/taketo1024/swm-khovanov.git",
-			from: "1.1.5"
+			from: "1.1.6"
 		),
         .package(
 			url: "https://github.com/taketo1024/swm-kr.git",
-			from: "0.3.3"
+			from: "0.4.0"
 		),
     ],
     targets: [
